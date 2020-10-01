1bcde7705232ab9804ae8e78b5cd52950cd1c547
Harry Parton <hello@harryparton.co.uk>
2020-10-01 18:15:49 +0200

    Tweak Error markup to remove the extra view container

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,14 +2,14 @@ import React, { useCallback } from 'react'
 import { Text, View } from 'react-native'
 import { useForm } from './Form'
 
-const Error = ({ as = View, children, name, ...props }) => {
+const Error = ({ as = Text, children, name, ...props }) => {
   const { errors } = useForm('Form.Error')
   const Tag = as
   const error = errors?.[name] || false
 
   return (
     <Tag {...{ ...props, error }}>
-      {children ? children(error) : error && <Text>{error.message}</Text>}
+      {children ? children(error) : error && error.message}
     </Tag>
   )
 }
