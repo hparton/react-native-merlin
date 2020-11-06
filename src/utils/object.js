af5bc12131ffbb3de08567511170ba02585a13c6
Harry Parton <hello@harryparton.co.uk>
2020-11-06 14:31:01 +0100

    #11 Add support for dot notation names.

--- src/utils/object.js
+++ src/utils/object.js
@@ -7,3 +7,32 @@ export const omit = (object = {}, keysToRemove = []) => {
     return result
   }, {})
 }
+
+export const get = (obj, keys) => {
+  if (!obj) return
+  if (typeof keys === 'string') return get(obj, keys.split('.'))
+  else if (keys.length === 0) return obj
+  else return get(obj[keys[0]], keys.slice(1))
+}
+
+// Set is currently from https://github.com/fwilkerson/clean-set
+// Slightly refactored since we don't need to be able to pass update functions
+export const set = (obj, keys, value) => {
+  keys.split && (keys = keys.split('.'))
+
+  let next = copy(obj)
+  let last = next
+  let totalKeys = keys.length
+
+  for (let i = 0; i < totalKeys; i++) {
+    last = last[keys[i]] = i === totalKeys - 1 ? value && value : copy(last[keys[i]])
+  }
+
+  return next
+}
+
+function copy(obj) {
+  let to = obj && !!obj.pop ? [] : {}
+  for (let i in obj) to[i] = obj[i]
+  return to
+}
